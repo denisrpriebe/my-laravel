34aaae8d5f3f7e766eab41c791e95cee25aa2a19
Denis Priebe <denis.priebe@irishtitan.com>
2019-08-15 15:51:08 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Shara
                 </div>
 
                 <div class="links">
